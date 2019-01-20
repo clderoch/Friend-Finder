50ffc6ad2ab2449da88d7ae4f3837325598a1797
Craig Derocher <craigderocher@Craig.local>
2019-01-19 21:08:31 -0600

    message

---
+++ app/data/friends.js
@@ -0,0 +1,39 @@
+var friends = [{
+        "name": "Britney Spears",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/8/84/Britney_Spears.jpg",
+        "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    },
+    {
+        "name": "Mark Zuckerberg",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/3/31/Mark_Zuckerberg_at_the_37th_G8_Summit_in_Deauville_018_v1.jpg",
+        "scores": [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+    },
+    {
+        "name": "Jerry Seinfeld",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/2/2c/Jerry_Seinfeld_by_David_Shankbone.jpg",
+        "scores": [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+    },
+    {
+        "name": "Donald Trump",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/5/53/Donald_Trump_official_portrait_%28cropped%29.jpg",
+        "scores": [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+    },
+    {
+        "name": "Darth Vader",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/b/bd/Star_Wars_A_Galaxy_Far%2C_Far_Away_Darth_Vader_4b.png",
+        "scores": [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+    },
+    {
+        "name": "Will Ferrell",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/c/c9/Will_Ferrell_LF_adjusted.jpg",
+        "scores": [3, 3, 4, 1, 4, 3, 5, 3, 5, 4]
+    },
+    {
+        "name": "Kurt Cobain",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/1/19/Nirvana_around_1992.jpg",
+        "scores": [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+    }
+
+]
+
+module.exports = friends;
